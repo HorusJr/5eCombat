a2423d5d9e498813a494b2da3450b90f80232c0e
Biraj Dahal <bdahal@g.clemson.edu>
2017-08-16 02:25:27 -0400

    Added enums and other abstractions

--- src/base/monster.py
+++ src/base/monster.py
@@ -1,26 +1,43 @@
+import random
 from enum import Enum
+from collections import *
+
+class Size(Enum):
+    TINY, SMALL, MEDIUM, LARGE, HUGE, GARGANTUAN = range(6)
+
+class Type(Enum):
+    ABERRATION, BEAST, CELESTIAL, CONSTRUCT, DRAGON, ELEMENTAL, FEY, FIEND, GIANT, HUMANOID, MONSTROSITY, OOZE, PLANT, UNDEAD = range(14)
+
+class Alignment(Enum):
+    LAWFULGOOD, NEUTRALGOOD, CHAOTICGOOD, LAWFULNEUTRAL, TRUENEUTRAL, CHAOTICNEUTRAL, LAWFULEVIL, NEUTRALEVIL, CHAOTICEVIL = range(9)
+
+class Language(Enum):
+    COMMON, DWARVISH, ELVISH, GIANT, GNOMISH, GOBLIN, ORC, ABYSSAL, CELESTIAL, DEEPSPEECH, DRACONIC, INFERNAL, PRIMORDIAL, SYLVAN, UNDERCOMMON, DRUIDIC, ARAKOCRA, \
+    BLINKDOG, BULLYWUG, GIANTEAGLE, GIANTELK, GIANTOWL, GITH, GNOLL, GRELL, HOOKHORROR, MODRON, OTYUGH, SLAAD, SPHINX, THRIKEEN, TROGLODYTE, UMBERHULK, WINTERWOLF, \
+    WORG, YETI = range(36)
+
+Abilities = namedtuple('Ability', ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'])
 
 class Monster:
     def __init__(self, name="Commoner"):
         self.name = name
-        self.size = "Medium"
-        self.type = "humanoid"
+        self.size = Size.MEDIUM
+        self.type = Type.HUMANOID
         self.tags = ("human")
-        self.alignment = "true neutral"
+        self.alignment = random.choice(list(Alignment))
         self.ac = 10
         self.hp = 4
-        self.speed = {'speed': 30, 'burrow': 0, 'climb': 15, 'fly': 0, 'swim': 15}
-        self.saving = ['STR': 10, 'DEX': 10, 'CON': 10, "INT": 10, "WIS": 10, "CHA": 10]
-        self.skills = ['athletics': 0, 'acrobatics': 0, 'sleight': 0, 'stealth': 0, 'arcana': 0, 'history': 0, 'investigation': 0, 'nature': 0, 'religion': 0 \
-            'animal': 0, 'insight': 0, 'medicine': 0, 'perception': 0, 'survival': 0, 'deception': 0, 'intimidation': 0, 'performance': 0, 'persuasion': 0]
-        self.saving = ['STR': 0, 'DEX': 0, 'CON': 0, "INT": 0, "WIS": 0, "CHA": 0]
+        self.abilities = Abilities(10,10,10,10,10,10)
+        self.speed = {'speed': 30}
+        self.skills = {}
+        self.throws = Abilities(0,0,0,0,0,0)
         self.vulnerabilities = ()
         self.resistances = ()
         self.immunities = ()
-        self.senses = {}
-        self.languages = ("Common")
+        self.senses = {'vision': -1}
+        self.languages = (Language.COMMON)
         self.cr = 0
 
         self.armor = ()
-        self.weapons = (Weapon())
+        self.weapons = ()
         self.equipment = ()
